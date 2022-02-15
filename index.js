883885fa6a766bb292cfdb9a94317a3ab19b7a33
Jorge <jhessle04@gmail.com>
2022-02-14 23:01:42 -0300

    N

---
+++ index.js
@@ -0,0 +1,63 @@
+ 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}         
+
+require('serve-favicon');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+const appointments = require('./routes/appointments');
+const scheduler = require('./scheduler');
+
+const app = express();
+require('./database');
+      
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// uncomment after placing your favicon in /public
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
+});          
+
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.locals.moment = require('moment');
+
+app.use('/appointments', appointments);
+app.use('/', appointments);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  console.error(err);
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {},
+  });
+});
+
+scheduler.start();
+
+ 
+module.exports = app;
